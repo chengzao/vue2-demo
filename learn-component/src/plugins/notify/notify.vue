0a238b568c5fbeb4f975399f802bae841d1a35af
chengzao <czhlink@163.com>
2019-05-22 17:36:05 +0800

    learn components

---
+++ learn-component/src/plugins/notify/notify.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{value}}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value: ''
+    }
+  },
+  components: {}
+}
+</script>
+
+<style scoped lang="stylus">
+h1 {
+  width: 100px;
+  height: 50px;
+  background-color: #000;
+  color: #fff;
+}
+</style>
